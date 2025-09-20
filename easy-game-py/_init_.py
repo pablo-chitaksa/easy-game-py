5cc232df5aa2388a97dcc3a64394b12f26a9f85b
pablo-chitaksa <solidarnostyoutub@gmail.com>
2025-09-20 09:45:59 +0300

    Create _init_.py

---
+++ easy-game-py/_init_.py
@@ -0,0 +1,14 @@
+"""
+:authors: pablo-chitaksa
+:license: Apache License, Version 2.0, see LICENSE file
+
+:copyright: (c) 2025 pablo_cheater228
+"""
+from .easygame import *
+from .enums import *
+from .exceptions import *
+from .utils import *
+
+_authors_ = 'pablo-chitaksa'
+_version_ = '0.0.1'
+_email_ = 'solidarnostyoutub@gmail.com'
